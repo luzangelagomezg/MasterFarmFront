efd7f8c6c04601885c3216fce1dc3796451f6a71
luzangelagomezg <118863660+luzangelagomezg@users.noreply.github.com>
2024-05-11 17:40:28 -0500

    add component animal list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AnimalListComponent } from "./animal/animal-list/animal-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "animals", component: AnimalListComponent},
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
