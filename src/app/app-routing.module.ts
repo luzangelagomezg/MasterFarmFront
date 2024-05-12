80e69fce7959f2abf14a05537c22608c42106fcc
luzangelagomezg <118863660+luzangelagomezg@users.noreply.github.com>
2024-05-11 22:16:51 -0500

    Add Farmer list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AnimalListComponent } from "./animal/animal-list/animal-list.component"
 import { ProducttypeListComponent } from "./product/producttype-list/producttype-list.component";
 import { ProducttypeCreateComponent } from "./product/producttype-create/producttype-create.component";
 import { ProductListComponent } from "./product/product-list/product-list.component";
+import { FarmerListComponent } from "./farm/farmer-list/farmer-list.component";
 
 const routes: Routes = [
     { path: "", redirectTo: "animals", pathMatch: "full" },
     { path: "animals", component: AnimalListComponent},
     { path: "producttypes", component: ProducttypeListComponent},
     { path: "producttypes/:id", component: ProducttypeCreateComponent},
-    { path: "products", component:ProductListComponent}
+    { path: "products", component:ProductListComponent},
+    { path: "farmers", component: FarmerListComponent}
 ];
 
 @NgModule({
---
+++ src/app/farm/farm.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FarmerListComponent } from './farmer-list/farmer-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    FarmerListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FarmModule { }
